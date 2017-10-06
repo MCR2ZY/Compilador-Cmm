999f969d79a27260318cc87a6c53329ccaa082f9
MCR2ZY <mauriciosm95@gmail.com>
2017-10-06 11:12:44 -0300

    Adiciona Enum com palavras reservadas da linguagem, com sinais da linguagem e com as categorias do token

--- compiladorCmm/analex.c
+++ compiladorCmm/analex.c
@@ -1,3 +1,23 @@
-#include "analex.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
+#include "analex.h"
+
+typedef enum palReservada{
+    PR_BOOLEANO,        PR_CARACTER,        PR_ENQUANTO, 
+    PR_INTEIRO,         PR_PARA,            PR_REAL, 
+    PR_RETORNE,         PR_SE,              PR_SEMRETORNO
+}PalReservada;
+
+typedef enum sinal{
+    SN_ABRI_CHAVE,          SN_ABRI_PARENTESE,       SN_AND,            SN_ATRIBUICAO,          SN_COMPARACAO,
+    SN_DIFERENTE,           SN_DIVISAO,              SN_DIVISAO,        SN_FECHA_CHAVE,         SN_FECHA_PARENTESE,
+    SN_MAIOR,               SN_MAIOR_IGUAL,          SN_MENOR,          SN_MENOR_IGUAL,         SN_MULTIPLICACAO,
+    SN_NEGACAO,             SN_OR,                   SN_PTO_VIRGULA,    SN_SOMA,                SN_SUBTRACAO
+}Sinal;
+
+typedef enum categoria{
+    CT_C, CT_I, CT_L, 
+    CT_R, ID,   PR, 
+    SINAL
+} Categoria;
