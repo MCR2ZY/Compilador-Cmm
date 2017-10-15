d53924c08f51099f310ceb3d389e8b7a86479424
MCR2ZY <mauriciosm95@gmail.com>
2017-10-14 23:12:24 -0300

    Adiciona ao arqiivo ".h", as bibliotecas, tabela de palavras reservadas, tabela de sinais, struct do token e protótipo da função analex.

--- compiladorCmm/analex.h
+++ compiladorCmm/analex.h
@@ -10,19 +10,31 @@
 #ifndef ANALEX_H_INCLUDED
 #define ANALEX_H_INCLUDED
 
+#include <stdin.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define QntPalReservadas 11
+#define TamPalReservadas 15
+#define QntSinal 21
+#define TamSinal 3
+#define TamLexema 20
+#define TamNum 20 
+#define EOS '\0'
+
 typedef enum palReservada{
     PR_BOOLEANO,        PR_CARACTER,        PR_ENQUANTO, 
     PR_INTEIRO,         PR_PARA,            PR_REAL, 
     PR_RETORNE,         PR_SE,              PR_SEM_PARAM,
-    PR_SEM_RETORNO,       PR_SENAO   
+    PR_SEM_RETORNO,     PR_SENAO   
 }PalReservada;
 
 typedef enum sinal{
     SN_ABRI_CHAVE,          SN_ABRI_PARENTESE,       SN_AND,            SN_ATRIBUICAO,          SN_COMPARACAO,
-    SN_DIFERENTE,           SN_DIVISAO,              SN_DIVISAO,        SN_FECHA_CHAVE,         SN_FECHA_PARENTESE,
-    SN_MAIOR,               SN_MAIOR_IGUAL,          SN_MENOR,          SN_MENOR_IGUAL,         SN_MULTIPLICACAO,
-    SN_NEGACAO,             SN_OR,                   SN_PTO_VIRGULA,    SN_SOMA,                SN_SUBTRACAO,
-    SN_VIRGULA
+    SN_DIFERENTE,           SN_DIVISAO,              SN_FECHA_CHAVE,    SN_FECHA_PARENTESE,     SN_MAIOR,       
+    SN_MAIOR_IGUAL,         SN_MENOR,                SN_MENOR_IGUAL,    SN_MULTIPLICACAO,       SN_NEGACAO,
+    SN_OR,                  SN_PTO_VIRGULA,          SN_SOMA,           SN_SUBTRACAO,           SN_VIRGULA
 }Sinal;
 
 typedef enum categoria{
@@ -30,5 +42,27 @@ typedef enum categoria{
     CT_R, ID,   PR, 
     SINAL
 } Categoria;
+
+extern char tabPalReservada[QntPalReservadas][TamPalReservadas]{
+    "booleano", "caracter", "enquanto", "inteiro", "para", "real", "retorne", "se", "semparam", "semretorno", "senao" 
+}
+
+extern char tabSinal[QntSinal][TamSinal]{
+    "{", "(", "&&", "=", "==", "!=", "/", "}", ")", ">", ">=", "<", "<=", "*", "!", "||", ";", "+", "-", ","
+}
+
+typedef struct token{
+    Categoria tipo;
+    char lexema[TamLexema];
+    union campo {
+		PalReservada	codPR;
+		Sinal			codSN;
+		int				valorInt;
+		double			valorReal;
+		int				posLiteral;
+	} valor;
+}Token;
+
+Token analex(FILE *);
 
 #endif // ANALEX_H_INCLUDED
