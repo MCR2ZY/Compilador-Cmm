3dc23995a982c3aa6ca2b963fa67bed96e19b369
MCR2ZY <mauriciosm95@gmail.com>
2017-10-14 23:16:30 -0300

    Declara variáveis token e descritor de arquivos. Adiciona abertura e fechamento do arquivo e chama a função analex.

--- compiladorCmm/main.c
+++ compiladorCmm/main.c
@@ -9,8 +9,38 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <analex.c>
 
 int main(int argc, char const *argv[])
 {
+    Token token;
+    FILE *fp;
+
+    if((fp = fopen (cod.txt, rt)) == NULL){
+        printf("ERRO AO ABRIR ARQUIVO");
+        printf("\n\tPressione <ENTER> para finalizar...\n\t");
+        getchar();
+        fflush(stdin);
+        exit(1);
+    }
+
+    while(!feof (fp)){
+        token = analex(fp);
+    }
+
+    printf ("\n\n\tAnalise lexica executada com sucesso.\n");
+	
+	printf("\n\tPressione <ENTER> para continuar...\n\t");
+	getchar();
+	fflush(stdin);
+    
+    if (fclose(fp)) {
+		printf ("\n\tErro fatal no fechamento de arquivo. O programa sera encerrado.\n");
+		printf("\n\tPressione <ENTER> para continuar...\n\t");
+		getchar();
+		fflush(stdin);
+		exit (1);
+	}
+
     return 0;
 }
