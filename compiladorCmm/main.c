09ec9a4caa4fdf24fb6799af0d787a990ea76a5b
MCR2ZY <mauriciosm95@gmail.com>
2017-10-19 14:42:22 -0300

    Checa qual o tipo de retorno no case END

--- compiladorCmm/main.c
+++ compiladorCmm/main.c
@@ -40,32 +40,44 @@ void imprimeToken(Token tkn){
 
     switch (tkn.tipo){
         case CT_C:
-            printf("\n\t< CT_C, \"%s\" >", tkn.lexema);;
+            printf("\n\t< CT_C, \" %s \" >", tkn.lexema);;
             break;
         case CT_I:
-            printf("\n\t< CT_I, \"%d\" >", tkn.valor.valorInt);
+            printf("\n\t< CT_I, \" %d \" >", tkn.valor.valorInt);
             break;
         case CT_L:
-            printf("\n\t< CT_L, \"%s\" >", tkn.lexema);
+            printf("\n\t< CT_L, \" %s \" >", tkn.lexema);
             break;
         case CT_R:
-            printf("\n\t< CT_R, \"%f\" >", tkn.valor.valorReal);
+            printf("\n\t< CT_R, \" %f \" >", tkn.valor.valorReal);
             break;
         case ID:
-            printf("\n\t< ID, \"%s\" >", tkn.lexema);
+            printf("\n\t< ID, \" %s \" >", tkn.lexema);
             printf(" [Codigo literal: %d]", tkn.valor.posLiteral);
             break;
         case PR:
-            printf("\n\t< PR, \"%s\" >", TabPalReservadas[tkn.valor.codPR]);
+            printf("\n\t< PR, \" %s \" >", TabPalReservadas[tkn.valor.codPR]);
             break;
         case SN:
-            printf("\n\t< SN, \"%s\" >", tkn.lexema);
+            printf("\n\t< SN, \" %s \" >", tkn.lexema);
             break;
         case END:
+            if(tkn.valor.valorInt == 1){
+            printf("\n\t< END, \"ERRO\" >");
             printf ("\n\n\tAnalise lexica executada com sucesso.\n");
             printf("\n\n\tEncerando...\n");
-            getchar();
             exit (1);
+            }
+            else{
+                printf("\n\t< END, \"FIM\" >");
+                printf ("\n\n\tAnalise lexica executada com sucesso.\n");
+                printf("\n\n\tEncerando...\n");
+            }
+            break;
+        default:
+            printf("\n\t< END, \"DEFAULT\" >");
+            printf ("\n\n\tAnalise lexica executada com sucesso.\n");
+            printf("\n\n\tEncerando...\n");
             break;
     }
 
